cc254aacf7991323dc79a92741d57b3e8f8e621d
bFraley <bFraley@users.noreply.github.com>
2016-10-11 16:34:21 -0400

    define Pizza and Topping models

--- restproject/pizza/models.py
+++ restproject/pizza/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Pizza(model.Model):
+    name = models.CharField(max_length=30)
+    price = models.CharField(max_length=10)
+
+class Topping(model.Model):
+    name = models.CharField(max_length=30)
+    price = models.CharField(max_length=10)
+
 
---
+++ restproject/pizza/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Pizza, Topping
+
+class PizzaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pizza
+        fields = ('name', 'price',)
+
+class ToppingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topping
+        fields = ('name', 'price',)
