6bdaa991be36e7eb76024e7527f5ce7502266cb3
bFraley <bFraley@users.noreply.github.com>
2016-10-12 10:27:09 -0400

    add pizza field to toppings serializer

--- restproject/pizza/serializers.py
+++ restproject/pizza/serializers.py
@@ -2,21 +2,22 @@ from rest_framework import serializers
 from .models import Pizza, Topping
 
 class PizzaSerializer(serializers.ModelSerializer):
-    pizza_count = serializers.SerializerMethodField()
+    #pizza_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Pizza
         fields = ('name', 'price',)
 
+
     #def get_pizza_count(self, pizza):
     #   return pizza.pizza_set.count()
 
 class ToppingSerializer(serializers.ModelSerializer):
-    topping_count = serializers.SerializerMethodField()
+    #topping_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Topping
-        fields = ('name', 'price',)
+        fields = ('name', 'price','pizza')
 
     #def get_topping_count(self, topping):
     #    return topping.topping_set.count()
